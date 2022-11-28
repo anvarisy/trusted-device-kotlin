e3aa709c7e42a9df8b70302d6b14800aa9d5dd1d
anvarisy <anvarisy@gmail.com>
2022-11-28 14:40:22 +0700

    Update v 1.0.4

--- app/src/main/java/com/fazpass/trusted_device/Fazpass.kt
+++ app/src/main/java/com/fazpass/trusted_device/Fazpass.kt
@@ -1,7 +1,6 @@
 package com.fazpass.trusted_device
 
 import android.content.Context
-import com.framgia.android.emulator.EmulatorDetector
 
 abstract class Fazpass : Base() {
     companion object{
@@ -10,15 +9,7 @@ abstract class Fazpass : Base() {
                 throw NullPointerException("merchant id cannot be null or empty")
             }
             Storage.storeDataLocal(context, "merchant_token", merchantToken)
-            Device(context)
-            EmulatorDetector.with(context)
-                .addPackageName("com.browserstack")
-                .setDebug(true)
-                .detect { isEmulator: Boolean ->
-                    if (isEmulator) {
-                        throw SecurityException("Device rooted or is an emulator lib stage")
-                    }
-                }
+//            Device(context)
 /*        SentryAndroid.init(context!!) { options: SentryAndroidOptions ->
             options.dsn =
                 "https://1f85de8be5544aaab7847e377b4c6227@o1173329.ingest.sentry.io/6720667"
