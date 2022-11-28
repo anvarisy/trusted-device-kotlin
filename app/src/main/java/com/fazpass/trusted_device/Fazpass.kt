e8e09c5928e3cbd10b81e67e79b73f654036d2a0
anvarisy <anvarisy@gmail.com>
2022-11-28 13:22:45 +0700

    Update fazpass

--- app/src/main/java/com/fazpass/trusted_device/Fazpass.kt
+++ app/src/main/java/com/fazpass/trusted_device/Fazpass.kt
@@ -4,24 +4,27 @@ import android.content.Context
 import com.framgia.android.emulator.EmulatorDetector
 
 abstract class Fazpass : Base() {
-    open fun initialize(context: Context, merchantToken: String?, mode: MODE?) {
-        if (merchantToken == null || merchantToken == "") {
-            throw NullPointerException("merchant id cannot be null or empty")
-        }
-        Storage.storeDataLocal(context, MERCHANT_TOKEN, merchantToken)
-        Device(context)
-        EmulatorDetector.with(context)
-            .addPackageName("com.browserstack")
-            .setDebug(true)
-            .detect { isEmulator: Boolean ->
-                if (isEmulator) {
-                    throw SecurityException("Device rooted or is an emulator lib stage")
-                }
+    companion object{
+        open fun initialize(context: Context, merchantToken: String?, mode: MODE?) {
+            if (merchantToken == null || merchantToken == "") {
+                throw NullPointerException("merchant id cannot be null or empty")
             }
+            Storage.storeDataLocal(context, "merchant_token", merchantToken)
+            Device(context)
+            EmulatorDetector.with(context)
+                .addPackageName("com.browserstack")
+                .setDebug(true)
+                .detect { isEmulator: Boolean ->
+                    if (isEmulator) {
+                        throw SecurityException("Device rooted or is an emulator lib stage")
+                    }
+                }
 /*        SentryAndroid.init(context!!) { options: SentryAndroidOptions ->
             options.dsn =
                 "https://1f85de8be5544aaab7847e377b4c6227@o1173329.ingest.sentry.io/6720667"
             options.tracesSampleRate = 1.0
         }*/
+        }
     }
+
 }
